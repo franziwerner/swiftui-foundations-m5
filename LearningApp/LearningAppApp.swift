a5812b1dba455c523dc4ce5d958b27a25209138d
Franziska Werner <franziwerner@gmx.de>
2021-10-22 10:33:10 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Franziska Werner on 22.10.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
